81abe3fba06a9402466cea66f7f255c7a03e6829
ValentinLutz <allround.email@gmail.com>
2023-06-18 00:21:20 +0200

    Update load tests

--- Makefile
+++ Makefile
@@ -4,12 +4,26 @@ help::
 	@egrep -h '\s##\s' $(MAKEFILE_LIST) \
 		| awk -F':.*?## | \\| ' '{printf "\033[36m%-38s \033[37m %-68s \033[35m%s \n", $$1, $$2, $$3}'
 
-export GOPROXY=direct
+# test.load:: ## Run load tests
+#	docker run -it \
+#		--rm \
+#		--volume ${PWD}/test-load:/k6 \
+#		--network host  \
+#       grafana/k6:0.44.1 \
+#		run /k6/script.js \
 
 test.load:: ## Run load tests
 	docker run -it \
 		--rm \
 		--volume ${PWD}/test-load:/k6 \
 		--network host  \
-        grafana/k6:0.44.1 \
-		run /k6/script.js \
+        ghcr.io/szkiba/xk6-dashboard:0.4.3 \
+		run --out json=/k6/test_result.json /k6/script.js
+
+test.report:: ## Run load tests
+	docker run -it \
+		--rm \
+		--volume ${PWD}/test-load:/k6 \
+		--network host  \
+        ghcr.io/szkiba/xk6-dashboard:0.4.3 \
+		dashboard replay /k6/test_result.json
--- test-load/script.js
+++ test-load/script.js
@@ -1,6 +1,6 @@
-import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import {randomString} from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import http from 'k6/http';
-import { check } from "k6";
+import {check} from "k6";
 
 export const BASE_URI = 'http://localhost:8080'
 
@@ -9,18 +9,28 @@ export const options = {
         http_req_duration: ['p(90) < 400', 'p(95) < 600'],
     },
     scenarios: {
-        full_scenario_shared: {
-            executor: 'shared-iterations',
-            vus: 100,
-            exec: 'fullScenario',
-            iterations: 2000,
-        },
+        // full_scenario_shared: {
+        //     executor: 'shared-iterations',
+        //     vus: 10,
+        //     exec: 'fullScenario',
+        //     iterations: 2000,
+        // },
         // full_scenario_constant: {
         //     executor: 'constant-vus',
         //     exec: 'fullScenario',
         //     vus: 400,
         //     duration: '30s',
         // },
+        full_scenario: {
+            executor: 'ramping-vus',
+            exec: 'fullScenario',
+            startVUs: 0,
+            stages: [
+                {duration: '1m', target: 100},
+                {duration: '1m', target: 0},
+            ],
+            gracefulRampDown: '20s',
+        },
     },
 };
 
@@ -33,7 +43,7 @@ export function fullScenario() {
 }
 
 export function getTasks() {
-    const response = http.get(BASE_URI + '/tasks');
+    const response = http.get(`${BASE_URI}/tasks`);
 
     check(response, {
         'getOrders is status 200': (r) => r.status === 200,
@@ -50,9 +60,12 @@ export function postTask() {
         headers: {
             'Content-Type': 'application/json',
         },
+        tags: {
+            name: `${BASE_URI}/tasks/{taskId}`
+        },
     };
 
-    const response = http.post(BASE_URI + '/tasks', payload, params);
+    const response = http.post(`${BASE_URI}/tasks`, payload, params);
 
     check(response, {
         'postTask is status 201': (r) => r.status === 201,
@@ -62,7 +75,13 @@ export function postTask() {
 }
 
 export function getTask(task_id) {
-    const response = http.get(BASE_URI + '/tasks/' + task_id);
+    const params = {
+        tags: {
+            name: `${BASE_URI}/tasks/{taskId}`
+        },
+    };
+
+    const response = http.get(`${BASE_URI}/tasks/${task_id}`, params);
 
     check(response, {
         'getTask is status 200': (r) => r.status === 200,
@@ -79,9 +98,12 @@ export function putTask(task_id) {
         headers: {
             'Content-Type': 'application/json',
         },
+        tags: {
+            name: `${BASE_URI}/tasks/{taskId}`
+        },
     };
 
-    const response = http.put(BASE_URI + '/tasks/' + task_id, payload, params);
+    const response = http.put(`${BASE_URI}/tasks/${task_id}`, payload, params);
 
     check(response, {
         'putTask is status 204': (r) => r.status === 204,
@@ -89,7 +111,13 @@ export function putTask(task_id) {
 }
 
 export function deleteTask(task_id) {
-    const response = http.del(BASE_URI + '/tasks/' + task_id);
+    const params = {
+        tags: {
+            name: `${BASE_URI}/tasks/{taskId}`
+        },
+    };
+
+    const response = http.del(`${BASE_URI}/tasks/${task_id}`, null, params);
 
     check(response, {
         'deleteTask is status 204': (r) => r.status === 204,
