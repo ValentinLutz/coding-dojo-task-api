932c37a8a82282060d5407812f5aaf4d35f20e5e
ValentinLutz <allround.email@gmail.com>
2023-06-18 16:27:10 +0200

    Refactor quarkus reactive implementation

--- java-quarkus-reactive/app/src/main/java/science/monke/incoming/TaskRequest.java
+++ java-quarkus-reactive/app/src/main/java/science/monke/incoming/TaskRequest.java
@@ -1,10 +1,12 @@
 package science.monke.incoming;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import science.monke.outgoing.Task;
 
 import java.util.UUID;
 
-public record TaskRequest(String title, String description) {
+public record TaskRequest(
+    @JsonProperty("title") String title, @JsonProperty("description") String description) {
   public static Task toTask(final TaskRequest taskRequest) {
     return toTask(UUID.randomUUID(), taskRequest);
   }
--- java-quarkus-reactive/app/src/main/java/science/monke/incoming/TaskResponse.java
+++ java-quarkus-reactive/app/src/main/java/science/monke/incoming/TaskResponse.java
@@ -1,10 +1,14 @@
 package science.monke.incoming;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import science.monke.outgoing.Task;
 
 import java.util.UUID;
 
-public record TaskResponse(UUID taskId, String title, String description) {
+public record TaskResponse(
+    @JsonProperty("task_id") UUID taskId,
+    @JsonProperty("title") String title,
+    @JsonProperty("description") String description) {
 
   public static TaskResponse fromTask(final Task task) {
     return new TaskResponse(task.taskId, task.title, task.description);
