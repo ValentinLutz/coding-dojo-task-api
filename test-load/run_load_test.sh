f54efc747efe25dd0e65c4e6c8996d0066412c21
ValentinLutz <allround.email@gmail.com>
2023-06-18 22:07:24 +0200

    Update load tests

--- test-load/run_load_test.sh
+++ test-load/run_load_test.sh
@@ -1,19 +1,23 @@
+#/bin/bash
+set -e
+
 app_name=$1
 use_memory=$2
 
-if [ $use_memory ]; then
-    script_name=script-memory.js
+if [ $use_memory ];
+then
+  test_id="$app_name-memory"
 else
-    script_name=script-postgres.js
+  test_id="$app_name-postgres"
 fi
-
-cp script.js $script_name
 
 export APP_NAME=$app_name
 export USE_MEMORY=$use_memory
 
 # start the app
-docker compose up -d --force-recreate --wait
+docker compose -f docker-compose.test.yaml up -d \
+  --force-recreate \
+  --wait
 # wait for the app to start
 sleep 30
 
@@ -26,13 +30,28 @@ sleep 30
 # 		grafana/k6:0.44.1 \
 # 		run --out=cloud /k6/$script_name
 
+#docker run -it \
+#   --rm \
+#   --volume ${PWD}:/k6 \
+#		--network host \
+#		grafana/k6:0.44.1 \
+#		run /k6/$script_name
+
 docker run -it \
-   --rm \
-   --volume ${PWD}:/k6 \
-		--network host \
-		grafana/k6:0.44.1 \
-		run /k6/$script_name
+  --rm \
+  --volume ${PWD}:/k6 \
+  --network host \
+  --env K6_INFLUXDB_ORGANIZATION=monke \
+  --env K6_INFLUXDB_BUCKET=coding_dojo \
+  --env K6_INFLUXDB_TOKEN=test \
+  --env K6_INFLUXDB_ADDR=http://localhost:8086 \
+  --env K6_INFLUXDB_CONCURRENT_WRITES=20 \
+  --env K6_INFLUXDB_PUSH_INTERVAL=10s \
+  xk6-influxdb:local \
+  run \
+  --tag testid=$test_id \
+  -o xk6-influxdb \
+  /k6/script.js
 
 # stop the app
-docker compose down \
-  --remove-orphans
+docker compose -f docker-compose.test.yaml down
--- test-load/run_load_tests.sh
+++ test-load/run_load_tests.sh
@@ -1,8 +1,18 @@
+#/bin/bash
+set -e
+
+docker compose -f docker-compose.yaml up -d \
+  --wait
+
+docker build \
+  -f xk6-influxdb/Dockerfile \
+  -t xk6-influxdb:local .
+
 for use_memory in "true" "false"
 do
   for app_name in "task-app-java-quarkus-reactive" "task-app-java-spring-web-mvc" "task-app-go-chi" "task-app-go-fiber"
   do
-    printf "run load test for app '$app_name' with memory '$use_memory'\n"
+    printf "Run load test for app '$app_name' with memory '$use_memory'\n"
     ./run_load_test.sh $app_name $use_memory
   done
 done
